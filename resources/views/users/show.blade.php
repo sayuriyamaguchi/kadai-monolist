f2bedba7a21244f75c703d7b5d2af7196b1bed14
sayuri.yamaguchi <sayuri.a.yamaguchi@rakuten.com>
2018-06-26 01:00:01 +0000

    have

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -19,7 +19,7 @@
                 <li>
                     <div class="status-label">HAVE</div>
                     <div id="have_count" class="status-value">
-                        xxx
+                         {{ $count_want }}
                     </div>
                 </li>
             </ul>
